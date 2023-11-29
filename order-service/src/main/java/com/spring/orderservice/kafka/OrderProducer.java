f6a8ce58f718bb602e091ed555f96cd15636ec16
hmalyala <m.hemanthkumar27@gmail.com>
2023-11-28 22:42:21 -0500

    Message Builder

--- order-service/src/main/java/com/spring/orderservice/kafka/OrderProducer.java
+++ order-service/src/main/java/com/spring/orderservice/kafka/OrderProducer.java
@@ -4,6 +4,7 @@ import com.spring.basedomains.dto.OrderEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
@@ -22,7 +23,10 @@ public class OrderProducer {
 
     public void sendMessage(OrderEvent orderEvent){
 
-        Message<OrderEvent> message = MessageBuilder.withPayload(orderEvent).build();
+        Message<OrderEvent> message = MessageBuilder
+                .withPayload(orderEvent)
+                .setHeader(KafkaHeaders.TOPIC, topic.name())
+                .build();
         kafkaTemplate.send(message);
     }
 }
